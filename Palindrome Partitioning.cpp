d7f6ff8d2ef44391eb05acb3c5ee429949f41f5d
ajayrammeena <55804608+ajayrammeena@users.noreply.github.com>
2023-03-01 01:10:20 +0530

    Update Palindrome Partitioning.cpp

--- Palindrome Partitioning.cpp
+++ Palindrome Partitioning.cpp
@@ -42,4 +42,33 @@ private:
         // cout<<endl;
     }
 };
-Time Complexity : O(N⋅2^N), where N is the length of string sss. This is the worst-case time complexity when all the possible substrings are palindrome.
+//Time Complexity : O(N⋅2^N), where N is the length of string sss. This is the worst-case time complexity when all the possible substrings are palindrome.
+    
+  //Using Dynamic Programing
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        int len = s.length();
+        vector<vector<bool>> dp (len, vector <bool> (len, false));
+        vector<vector<string>> result;
+        vector<string> currentList;
+        dfs(result, s, 0, currentList, dp);
+        return result;
+    }
+
+    void dfs(vector<vector<string>> &result, string &s, int start, vector<string> &currentList, vector<vector<bool>> &dp) {
+        if (start >= s.length()) result.push_back(currentList);
+        for (int end = start; end < s.length(); end++) {
+            if (s[start] == s[end] && (end - start <= 2 || dp[start + 1][end - 1])) {
+                dp[start][end] = true;
+                currentList.push_back(s.substr(start, end - start + 1));
+                dfs(result, s, end + 1, currentList, dp);
+                currentList.pop_back();
+            }
+        }
+    }
+};
+//A given string s starting at index start and ending at index end is a palindrome if the following conditions are satisfied:
+
+//         The characters at start and end indexes are equal.
+//         The substring starting at index start+1 and ending at index end−1 is a palindrome.
